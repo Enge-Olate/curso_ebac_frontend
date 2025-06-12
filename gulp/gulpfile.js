3ffe135efbcb52c66a80651365583ee388ab6fb0
Enge-Olate <marcioalexisolate@live.com>
2025-06-12 10:19:23 -0300

    Refactor Gulp configuration and update image assets
    - Changed Gulp script to remove watch mode.
    - Deleted multiple outdated image files.
    - Added new image assets including birds, butterflies, and hands.
    - Removed an unused WebP image.

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,38 +1,51 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')( require('sass'));
-const obfuscate = require('gulp-obfuscate');
-const uglify = require('gulp-uglify');
-const image = require('gulp-imagemin');
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const obfuscate = require("gulp-obfuscate");
+const uglify = require("gulp-uglify");
+const image = require("gulp-imagemin");
+const source_maps = require('gulp-sourcemaps');
 
+function compilaSass() {
+  return gulp
+    .src("./src/styles/*.scss")
+	.pipe(source_maps.init())
+    .pipe(
+      sass({ style: "compressed" }, { ignoreInitial: false }).on(
+        "error",
+        sass.logError
+      )
+    )
+	.pipe(source_maps.write('./maps'))
+    .pipe(gulp.dest("./build/styles/"));
+}
 
-function compilaSass(){
-	return gulp.src('./src/styles/*.scss')
-	.pipe(sass({style: 'compressed'},{ignoreInitial: false}).on('error', sass.logError))
-	.pipe(gulp.dest('./build/styles/'))
+function javascriptLindo() {
+  return gulp
+    .src("./src/scripts/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./build/scripts/"));
+}
+
+function obfuscateJavascript() {
+  return (
+    gulp
+      .src("./src/scripts/*.js")
+      .pipe(obfuscate())
+      .pipe(gulp.dest("./build/scripts/")),
+    javascriptLindo()
+  );
+}
+
+function comprimeImagens() {
+  return gulp
+    .src("./src/imagens/*")
+    .pipe(image())
+    .pipe(gulp.dest("./build/imagens_min/"));
+}
+
+exports.watch = function () {
+  gulp.watch("./src/styles/*scss",  gulp.series(compilaSass));
+  gulp.watch("./src/scripts/*.js", {ignoreInitial: false}, gulp.series(obfuscateJavascript));
+  gulp.watch("./src/imagens/*", {ignoreInitial:false}, gulp.series(comprimeImagens));
 };
-
-function javascriptLindo(){
-	return gulp.src('./src/scripts/*.js')
-	.pipe(uglify())
-	.pipe(gulp.dest('./build/scripts/')) 
-}
-
-function obfuscateJavascript(){
-	return gulp.src('./src/scripts/*.js')
-	.pipe(obfuscate())
-	.pipe(gulp.dest('./build/scripts/')),
-	javascriptLindo();
-};
-
-function comprimeImagens(){
-	return gulp.src('./src/imagens/*')
-	.pipe(image())
-	.pipe(gulp.dest('./build/imagens/'))
-}
-
-exports.watch = function (){
-	gulp.watch('./src/styles/*scss', compilaSass);
-	gulp.watch('./src/scripts/*.js', obfuscateJavascript);
-	gulp.watch('./src/imagens/*', comprimeImagens);
-}
 exports.default = this.watch;
