04d58a517bc037a29345084a6b70d027193d4a3a
Enge-Olate <marcioalexisolate@live.com>
2025-06-18 11:57:09 -0300

    npm ins grunt-contrib-less grunt-replace, gruntfile.js less e build.

--- grunt/gruntfile.js
+++ grunt/gruntfile.js
@@ -1,5 +1,28 @@
 module.exports= function (grunt){
     grunt.initConfig({
-        pkg:grunt.file.readJSON('package.json')
+        pkg:grunt.file.readJSON('package.json'),
+        less:{
+            development:{
+                files:{
+                    'dev/styles/main.css':'src/styles/main.less'
+                }
+            },
+            production:{
+                options:{
+                    compress: true,
+                },
+                files:{
+                    'dist/styles/main.css': 'src/styles/main.less'
+                }
+            }
+        },
+        
     });
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.registerTask("minha_tarefa", function(){
+        console.log('Hello, Grunt.');
+    });
+    grunt.registerTask('default',["minha_tarefa"]);
+    grunt.registerTask('build',["less:production", "replace:dist"]);
 }
