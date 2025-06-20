d370510b359f8d576d331ff28932b58ad2a7de9e
Enge-Olate <marcioalexisolate@live.com>
2025-06-20 13:17:32 -0300

    Minificando main.min.css e index.html.

--- grunt/gruntfile.js
+++ grunt/gruntfile.js
@@ -17,6 +17,57 @@ module.exports= function (grunt){
                 }
             }
         },
+
+        replace:{
+            dev:{
+                options:{
+                    patterns:[
+                        {
+                            match: 'ENDERECO_CSS',
+                            replacement: './styles/main.css'   
+                        }
+                    ]
+                },
+                files:[
+                    {
+                        expand:true,
+                        flatten: true,
+                        src:['views/index.html'],
+                        dest: 'dev/'
+                    }
+                ]
+            },
+            dist:{
+                options:{
+                    patterns:[
+                        {
+                            match: 'ENDERECO_CSS',
+                            replacement: './styles/main.min.css'   
+                        }
+                    ]
+                },
+                files:[
+                    {
+                        expand:true,
+                        flatten: true,
+                        src:['prebuild/index.html'],
+                        dest: 'dist/'
+                    }
+                ]
+            }
+        },
+        htmlmin:{
+            dist:{
+                options:{
+                    removeComments:true,
+                    collapseWhitespace:true
+                },
+                files:{
+                    'prebuild/index.html':'views/index.html'
+                }
+            }
+        },
+
         watch:{
             less:{
                 files:['src/styles/*.less'],
@@ -28,10 +79,11 @@ module.exports= function (grunt){
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+    // grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-replace');
     grunt.registerTask("minha_tarefa", function(){
         console.log('Trabalhando...');
     });
     grunt.registerTask('default',["minha_tarefa", "watch"]);
-    grunt.registerTask('build',["less:production"]);
+    grunt.registerTask('build',["less:production", 'htmlmin:dist', "replace:dist" ]);
 }
