9e93f1f5992f0c12af75e618a5bc560b073e21d5
Enge-Olate <marcioalexisolate@live.com>
2025-06-21 14:27:27 -0300

    Exercício Grunt.

--- grunt/gruntfile.js
+++ grunt/gruntfile.js
@@ -18,96 +18,26 @@ module.exports= function (grunt){
             }
         },
 
-        replace:{
-            dev:{
-                options:{
-                    patterns:[
-                        {
-                            match: 'ENDERECO_CSS',
-                            replacement: './styles/main.css'   
-                        },
-                        {
-                            match: 'ENDERECO_JS',
-                            replacement: './scripts/main.js'
-                        }
-                    ]
-                },
-                files:[
-                    {
-                        expand:true,
-                        flatten: true,
-                        src:['views/index.html'],
-                        dest: 'dev/'
-                    }
-                ]
-            },
-            dist:{
-                options:{
-                    patterns:[
-                        {
-                            match: 'ENDERECO_CSS',
-                            replacement: './styles/main.min.css'   
-                        },
-                        {
-                            match: 'ENDERECO_JS',
-                            replacement: './scripts/saida.maim.min.js'
-                        }
-                    ]
-                },
-                files:[
-                    {
-                        expand:true,
-                        flatten: true,
-                        src:['prebuild/index.html'],
-                        dest: 'dist/'
-                    }
-                ]
-            }
-        },
-        htmlmin:{
-            dist:{
-                options:{
-                    removeComments:true,
-                    collapseWhitespace:true
-                },
-                files:{
-                    'prebuild/index.html':'views/index.html'
-                }
-            }
-        },
-        clean:['prebuild'],
-
+        
         uglify:{
             target:{
                 files:{
 
-                    'dist/scripts/saida.main.min.js': 'src/scripts/*.js',
+                    'dist/scripts/main.min.js': 'src/scripts/*.js',
                     'dev/scripts/main.js':'src/scripts/*js'
                 
                 }
             }
         },
-        watch:{
-            less:{
-                files:['src/styles/*.less'],
-                tasks:['less:development']
-            },
-            html:{
-                files:['views/index.html'],
-                tasks:['replace:dev']
-            }
-        }
+        
         
     });
     grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-htmlmin');
-    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-replace');
+   
     grunt.registerTask("minha_tarefa", function(){
         console.log('Trabalhando...');
     });
-    grunt.registerTask('default',["minha_tarefa", "watch"]);
-    grunt.registerTask('build',["less:production", 'htmlmin:dist', "replace:dist", "clean", "uglify"]);
+    grunt.registerTask('default',["minha_tarefa"]);
+    grunt.registerTask('build',["less:production", "uglify"]);
 }
