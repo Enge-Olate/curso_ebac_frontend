7f3b11242090d2c507af2ae6635043a3b765fe58
Enge-Olate <marcioalexisolate@live.com>
2025-06-21 13:56:21 -0300

    Lógica sorteio.

--- grunt/src/scripts/sorteia_num.js
+++ grunt/src/scripts/sorteia_num.js
@@ -1,22 +1,29 @@
 function pega_valor() {
     const numero = document.getElementById('number').value;
-    return numero;
+    let number = parseInt(numero);
+
+    return Math.floor(Math.random() * number);
 }
 function cria_p() {
     const p = document.createElement('p');
     p.innerHTML = `O número sorteado foi: ${pega_valor()}`;
-
-
     return p;
 }
 
 function carrega_dado() {
+    let count = 0;
     const botao = document.getElementById('botao');
     botao.addEventListener('click', function (e) {
         e.preventDefault();
         const div_resultado = document.getElementsByClassName('resultado')[0];
-        div_resultado.append(cria_p());
-        // alert('ola');
+        count++;
+        if ((count % 2 == 0)) {
+            div_resultado.innerHTML = '';
+        } else {
+            div_resultado.append(cria_p());
+
+        }
+        !pega_valor() ? div_resultado.innerHTML = '' : div_resultado;
     })
 
 }
