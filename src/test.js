883a9c11906e3146e097c8e9ead1b75a48d8deb3
Enge-Olate <marcioalexisolate@live.com>
2025-09-23 11:32:07 -0300

    aplicando exercício typescript.

---
+++ src/test.js
@@ -0,0 +1,8 @@
+function name(nome) {
+    return "Ol\u00E1, ".concat(nome);
+}
+function multiplicar(number1, number2) {
+    return number1 * number2;
+}
+console.log(name('Márcio'));
+console.log(multiplicar(10, 20));
---
+++ src/test.ts
@@ -0,0 +1,9 @@
+
+function name(nome:String){
+    return `Olá, ${nome}`;
+}
+function multiplicar(number1:number, number2:number):number{
+    return number1 * number2;
+}
+console.log(name('Márcio'));
+console.log(multiplicar(10, 20));
